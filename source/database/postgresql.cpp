3f7e39819a0f476316095a78dbdd119dd2ac53e4
Alex Yagni <near1tfeex@gmail.com>
2025-02-02 22:43:28 +0500

    refactor(PostgreSQL): Move implementation from header to source file

--- source/database/postgresql.cpp
+++ source/database/postgresql.cpp
@@ -1,1 +1,256 @@
 #include "postgresql.h"
+
+
+PostgreSQL::PostgreSQL(
+    std::string_view host,
+    std::string_view user,
+    std::string_view pass,
+    std::string_view dbName,
+    int port = 5432,
+    int countConn = 1
+)
+    : m_config{ host, user, pass, dbName, port }
+    , m_pool{ countConn }
+{
+
+}
+
+
+void PostgreSQL::Connect() {
+    const char* keywords[] = { "host", "user", "password", "dbname", "port", nullptr };
+    const char* values[] = { m_config.GetHost().c_str(), 
+        m_config.GetUser().c_str(), 
+        m_config.GetPassword().c_str(),
+        m_config.GetDatabaseName().c_str(),
+        m_config.GetPort().c_str(),
+        nullptr };
+
+    m_pool.Connect(keywords, values);
+}
+
+
+void PostgreSQL::Disconnect() {
+    m_pool.Disconnect();
+}
+
+
+std::vector<int>  PostgreSQL::GetLengthsParams(SqlParams params) {
+    std::vector<int> lengths{ };
+    lengths.reserve(params.size());
+    for (const auto& param : params) {
+        if (param.second.empty()) {
+            lengths.emplace_back(STR_NULL.size());
+        }
+        else {
+            lengths.emplace_back(static_cast<int>(param.second.size()));
+        }
+    }
+
+    return lengths;
+}
+
+
+std::vector<const char*>  PostgreSQL::GetValuesParams(SqlParams params) {
+    std::vector<const char*> values{ };
+    values.reserve(params.size());
+    for (const auto& param : params) {
+        if (param.second.empty()) {
+            values.emplace_back(STR_NULL);
+        }
+        else {
+            values.push_back(param.second.c_str());
+        }
+    }
+
+    return values;
+}
+
+
+void PostgreSQL::Execute(std::string_view query, SqlParams params) {
+    auto lengths{ GetLengthsParams(params) };
+    auto values{ GetValuesParams(params) };
+
+    auto conn{ m_pool.Acquire() };
+
+    PGresultPtr resGuard(
+        PQexecParams(conn.get(),
+            query.data(),
+            params.size(),
+            nullptr,
+            values.data(),
+            lengths.data(),
+            nullptr,
+            0), PQclear);
+
+
+    std::string msg_error{ PQerrorMessage(conn.get()) };
+    m_pool.Release(std::move(conn));
+    if (PQresultStatus(resGuard.get()) != PGRES_COMMAND_OK) {
+        throw PostgreSQLError::ExecuteError(msg_error);
+    }
+}
+
+
+std::vector<std::string> PostgreSQL::ReadPostgresResult(PGresultPtr resGuard) {
+    std::vector<std::string> data{ };
+    if (PQntuples(resGuard.get()) != 0) {
+        int rows = PQntuples(resGuard.get());
+        int cols = PQnfields(resGuard.get());
+        data.reserve(rows * cols);
+        for (int i{ 0 }; i < rows; ++i) {
+            for (int j{ 0 }; j < cols; ++j) {
+                std::string str{ PQgetvalue(resGuard.get(), i, j) };
+                if (str.empty()) {
+                    str = STR_NULL;
+                }
+
+                data.emplace_back(std::move(str));
+            }
+        }
+    }
+
+    return data;
+}
+
+
+std::vector<std::string> PostgreSQL::ExecuteQuery(std::string_view query, SqlParams params) {
+    auto lengths{ GetLengthsParams(params) };
+    auto values{ GetValuesParams(params) };
+
+    auto conn{ m_pool.Acquire() };
+    PGresultPtr resGuard{ PQexecParams(conn.get(),
+            query.data(),
+            params.size(),
+            nullptr,
+            values.data(),
+            lengths.data(),
+            nullptr,
+            0), PQclear };
+
+
+    std::string msg_error{ PQerrorMessage(conn.get()) };
+    m_pool.Release(std::move(conn)); 
+    if (PQresultStatus(resGuard.get()) != PGRES_TUPLES_OK) {
+        throw PostgreSQLError::ExecuteError(msg_error);
+    }
+
+    return ReadPostgresResult(std::move(resGuard));
+}
+
+
+void PostgreSQL::BeginTransaction() {
+    //Not necessary yet
+    throw std::runtime_error("Missing implementation");
+}
+
+
+void PostgreSQL::CommitTransaction() {
+    // Not necessary yet
+    throw std::runtime_error("Missing implementation");
+}
+
+
+void PostgreSQL::RollbackTransaction() {
+    // Not necessary yet
+    throw std::runtime_error("Missing implementation");
+}
+
+
+PostgreSQL::ConnectionPool::ConnectionPool(int countConn)
+    : m_countConn{ countConn }
+{
+    if (countConn < 1) {
+        throw PostgreSQLError::ConnectionPoolError("Number of connections must be >= 1.");
+    }
+}
+
+
+void PostgreSQL::ConnectionPool::Connect(const char* keywords[], const char* values[]) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+
+    m_connections.clear();
+    for (int i = 0; i < m_countConn; ++i) {
+        PGconnPtr conn{ PQconnectdbParams(keywords, values, 0), 
+            &PQfinish };
+
+        if (PQstatus(conn.get()) != CONNECTION_OK) {
+            m_connections.clear();
+            throw PostgreSQLError::ConnectError(PQerrorMessage(conn.get()));
+        }
+
+        m_connections.emplace_back(std::move(conn));
+    }
+}
+
+
+void PostgreSQL::ConnectionPool::Disconnect() {
+    std::lock_guard<std::mutex> lock(m_mutex);
+
+    for (auto& conn : m_connections) {
+        if (conn.get() != nullptr) {
+            PQfinish(conn.get());
+        }
+    }
+
+    m_connections.clear();
+}
+
+
+PostgreSQL::PGconnPtr PostgreSQL::ConnectionPool::Acquire() {
+    std::unique_lock<std::mutex> lock{ m_mutex };
+    while (m_connections.empty()) {
+        m_cond.wait(lock);
+    }
+
+    auto conn{ std::move(m_connections.back()) };
+    m_connections.pop_back();
+
+    return conn;
+}
+
+
+void PostgreSQL::ConnectionPool::Release(PGconnPtr conn) {
+    if (PQstatus(conn.get()) != CONNECTION_OK) {
+        PQreset(conn.get());
+        if (PQstatus(conn.get()) != CONNECTION_OK) {
+            throw PostgreSQLError::ResetError(PQerrorMessage(conn.get()));
+        }
+    }
+
+    Push(std::move(conn));
+
+    m_cond.notify_one();
+}
+
+
+PostgreSQL::ConnectionConfig::ConnectionConfig(std::string_view host,
+    std::string_view user,
+    std::string_view pass,
+    std::string_view dbName,
+    int port)
+    : m_host{ host }
+    , m_user{ user }
+    , m_pass{ pass }
+    , m_dbName{ dbName }
+    , m_port{ std::to_string(port) }
+{
+    if (m_host.empty()) {
+        throw PostgreSQLError::ConnectionConfigError("Field host cannot be empty.");
+    }
+
+    if (m_user.empty()) {
+        throw PostgreSQLError::ConnectionConfigError("Field user cannot be empty.");
+    }
+
+    if (m_pass.empty()) {
+        throw PostgreSQLError::ConnectionConfigError("Field password cannot be empty.");
+    }
+
+    if (m_dbName.empty()) {
+        throw PostgreSQLError::ConnectionConfigError("Field dbname cannot be empty.");
+    }
+
+    if (port < 0 || port > 65535) {
+        throw PostgreSQLError::ConnectionConfigError("Field port must be a valid port number between 0 and 65535.");
+    }
+}
--- source/database/postgresql.h
+++ source/database/postgresql.h
@@ -13,224 +13,77 @@
 
 
 #include "IDatabase.h"
+#include "postgresqlError.h"
 
 
 class PostgreSQL : public IDatabase {
 public:
 
-    PostgreSQL(
-        std::string_view host,
+    using PGresultPtr = std::unique_ptr<PGresult, decltype(&PQclear)>;
+    using PGconnPtr = std::unique_ptr<PGconn, decltype(&PQfinish)>;
+
+    PostgreSQL(std::string_view host,
         std::string_view user,
         std::string_view pass,
-        std::string_view dbName, 
-        int port = 5432, 
-        int countConn = 1
-    )
-        : m_config{ host, user, pass, dbName, port }
-        , m_pool{ countConn }
-    {
+        std::string_view dbName,
+        int port = 5432,
+        int countConn = 1);
 
-    }
+    void Connect() override;
 
-    void Connect() override {
-        const char* keywords[] = { "host", "user", "password", "dbname", "port", nullptr };
-        const char* values[] = { m_config.GetHost().c_str(),
-                                 m_config.GetUser().c_str(), 
-                                 m_config.GetPassword().c_str(), 
-                                 m_config.GetDatabaseName().c_str(), 
-                                 m_config.GetPort().c_str(), 
-                                 nullptr };
+    void Disconnect() override;
 
-        m_pool.Connect(keywords, values);
-    }
+    void Execute(std::string_view query, SqlParams params) override;
 
-    void Disconnect() override {
-        m_pool.Disconnect();
-    }
+    std::vector<std::string> ExecuteQuery(std::string_view query, SqlParams params) override;
 
-    void Execute(std::string_view query, SqlParams params) override {
+    void BeginTransaction() override;
 
-        auto lengths{ GetLengthsParams(params) };
-        auto values{ GetValuesParams(params) };
+    void CommitTransaction() override;
 
-        auto conn{ m_pool.Acquire() };
-
-        std::unique_ptr<PGresult, decltype(&PQclear)> res(
-            PQexecParams(conn.get(), 
-                         query.data(), 
-                         params.size(), 
-                         nullptr, 
-                         values.data(), 
-                         lengths.data(), 
-                         nullptr, 
-                         0), PQclear);
-
-        if (PQresultStatus(res.get()) != PGRES_COMMAND_OK) {
-            m_pool.Release(std::move(conn));
-            return;
-        }
-
-        m_pool.Release(std::move(conn));
-    }
-
-    std::vector<std::string> ExecuteQuery(std::string_view query, SqlParams params) override {
-
-        auto lengths{ GetLengthsParams(params) }; // Нужно добавить Проверку на null
-        auto values{ GetValuesParams(params) };
-
-        auto conn{ m_pool.Acquire() };
-        std::unique_ptr<PGresult, decltype(&PQclear)> res_guard{
-            PQexecParams(conn.get(),
-                query.data(),
-                params.size(),
-                nullptr,
-                values.data(),
-                lengths.data(),
-                nullptr,
-                0), PQclear };
-
-        if (PQresultStatus(res_guard.get()) != PGRES_TUPLES_OK) {
-            m_pool.Release(std::move(conn));
-            return { };
-        }
-
-        m_pool.Release(std::move(conn));
-
-        std::vector<std::string> data{ };
-        if (PQntuples(res_guard.get()) != 0) {
-            int rows = PQntuples(res_guard.get());
-            int cols = PQnfields(res_guard.get());
-            data.reserve(rows * cols);
-            for (int i{ 0 }; i < rows; ++i) {
-                for (int j{ 0 }; j < cols; ++j) {
-                    data.emplace_back(
-                        PQgetvalue(res_guard.get(), i, j));
-                }
-            }
-        }
-
-        return data;
-    }
-
-    void BeginTransaction() override {
-        //Not necessary yet
-        throw std::exception("Missing implementation");
-    }
-
-    void CommitTransaction() override {
-        // Not necessary yet
-        throw std::exception("Missing implementation");
-    }
-
-    void RollbackTransaction() override {
-        // Not necessary yet
-        throw std::exception("Missing implementation");
-    }
+    void RollbackTransaction() override;
 
     ~PostgreSQL() = default;
 
 private:
 
-    static std::vector<int> GetLengthsParams(SqlParams params) {
-        std::vector<int> lengths{ };
-        for (const auto& param : params) {
-            lengths.emplace_back(static_cast<int>(param.second.size()));
-        }
+    const std::string STR_NULL{ "NULL" };
+    std::vector<int> GetLengthsParams(SqlParams params);
+    std::vector<const char*> GetValuesParams(SqlParams params);
 
-        return lengths;
-    }
-
-    static std::vector<const char*> GetValuesParams(SqlParams params) {
-        std::vector<const char*> values{ };
-        for (const auto& param : params) {
-            values.emplace_back(param.second.c_str());
-        }
-
-        return values;
-    }
+    std::vector<std::string> ReadPostgresResult(PGresultPtr resGuard);
 
 private:
 
     class ConnectionPool {
     public:
 
-        ConnectionPool(int countConn)
-            : m_countConn{ countConn }
-        {
-            if (countConn < 1) {
-                throw std::invalid_argument("Number of connections must be >= 1.");
-            }
-        }
+        ConnectionPool(int countConn);
 
-        void Connect(const char* keywords[], const char* values[]) {
-            std::lock_guard<std::mutex> lock(m_mutex);
+        void Connect(const char* keywords[], const char* values[]);
 
-            m_connections.clear();
-            for (int i = 0; i < m_countConn; ++i) {
-                PGconn* conn{ PQconnectdbParams(keywords, values, 0) };
-                if (PQstatus(conn) != CONNECTION_OK) {
-                    std::string message = PQerrorMessage(conn);
-                    std::cerr << "Connection failed: " << message << "\n";
-                    PQfinish(conn);
-                    return;
-                }
-
-                m_connections.emplace_back(conn, &PQfinish);
-            }
-        }
-
-        void Disconnect() {
-            for (auto& conn : m_connections) {
-                if (conn.get() != nullptr) {
-                    PQfinish(conn.get());
-                }
-            }
-
-            m_connections.clear();
-        }
+        void Disconnect();
 
         int Count() const { return m_connections.size(); }
 
-        std::unique_ptr<PGconn, decltype(&PQfinish)> Acquire() {
-            std::unique_lock<std::mutex> lock{ m_mutex };
-            while (m_connections.empty()) {
-                m_cond.wait(lock);
-            }
+        PGconnPtr Acquire();
 
-            auto conn{ std::move(m_connections.back()) };
-            m_connections.pop_back();
-
-            return conn;
-        }
-
-        void Release(std::unique_ptr<PGconn, decltype(&PQfinish)> conn) {
-            if (PQstatus(conn.get()) != CONNECTION_OK) {
-                PQreset(conn.get());
-                if (PQstatus(conn.get()) != CONNECTION_OK) {
-                    throw std::exception(PQerrorMessage(conn.get()));
-                }
-            }
-
-            Push(std::move(conn));
-
-            m_cond.notify_one();
-        }
+        void Release(PGconnPtr conn);
 
     private:
 
-        void Push(std::unique_ptr <PGconn, decltype(&PQfinish)> conn) {
+        void Push(PGconnPtr conn) {
             std::lock_guard<std::mutex> lock(m_mutex);
             m_connections.push_back(std::move(conn));
         }
 
     private:
         int m_countConn{ };
-        std::vector<std::unique_ptr<PGconn, decltype(&PQfinish)>> m_connections;
+        std::vector<PGconnPtr> m_connections;
 
         std::mutex m_mutex{ };
         std::condition_variable m_cond{ };
     };
-
 
     class ConnectionConfig {
     public:
@@ -238,33 +91,7 @@ private:
             std::string_view user,
             std::string_view pass,
             std::string_view dbName,
-            int port) 
-            : m_host{ host }
-            , m_user{ user }
-            , m_pass{ pass }
-            , m_dbName{ dbName }
-            , m_port{ std::to_string(port) }
-        {
-            if (m_host.empty()) {
-                throw std::invalid_argument("Field host cannot be empty.");
-            }
-
-            if (m_user.empty()) {
-                throw std::invalid_argument("Field user cannot be empty.");
-            }
-
-            if (m_pass.empty()) {
-                throw std::invalid_argument("Field password cannot be empty.");
-            }
-
-            if (m_dbName.empty()) {
-                throw std::invalid_argument("Field dbname cannot be empty.");
-            }
-
-            if (port < 0 || port > 65535) {
-                throw std::invalid_argument("Field port must be a valid port number between 0 and 65535.");
-            }
-        }
+            int port);
 
         const std::string& GetHost() const { return m_host; }
         const std::string& GetUser() const { return m_user; }
@@ -286,5 +113,3 @@ private:
     ConnectionConfig m_config;
     ConnectionPool m_pool;
 };
-
-// Создать свои исключения для posgreSQL
